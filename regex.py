13fa064a0d0cf627989c07b04775c66477dc886f
John Willard <jgwil2@gmail.com>
2019-06-18 22:39:13 -0400

    rip out bad code

--- regex.py
+++ regex.py
@@ -20,76 +20,49 @@ class NFA(object):
         '''
         Compile an NFA given a regular expression.
         '''
-        self.frag_stack = []
 
         for c in regex:
             if c == '(':
                 pass
-            if c == ')':
+            elif c == ')':
                 pass
-            if c == '|':
+            elif c == '|':
                 self.alternate()
-            if c == '*':
+            elif c == '*':
                 self.star()
-            if c == '+':
+            elif c == '+':
                 self.plus()
-            if c == '?':
+            elif c == '?':
                 self.question()
             else:
-                # if there is something in the frag stack then we pop
-                # it off the stack and patch all its out arrows to
-                # point to the new frag's start state and push the new
-                # fragment back onto the stack
-                current = self.literal(c)
-                if len(self.frag_stack) > 0:
-                    previous = self.frag_stack.pop()
-                    current = self.concat(previous, current)
-
-                # FIXME
-                # otherwise this is the first character we are reading,
-                # so we simply push the fragment onto the stack and
-                # track this fragment's start state
-                self.start_state = current.start_state
-                self.frag_stack.append(current)
-
-        # at the end of the loop, we should create a match state
-        # and connect it to remaining fragment(s?) in the stack
-        final_state = State(None, None, True)
-        frag = self.frag_stack.pop()
-        self.patch(frag.out_arrows, final_state)
-
-        self.start_state = frag.start_state
+                pass
 
     def literal(self, c):
         '''
-        Returns fragment for literal character
+        Returns an NFA for a literal character
         '''
-        arrow = Arrow(c)
-        return Fragment(State(arrow), [arrow])
+        pass
 
     def concat(self, f1, f2):
         '''
         Concatenation
-        Returns a fragment connecting the out arrows of f1 to the
+        Returns an NFA connecting the out arrows of f1 to the
         start state of f2
         '''
-        self.patch(f1.out_arrows, f2.start_state)
-        return Fragment(f1.start_state, f2.out_arrows)
+        pass
 
     def alternate(self, f1, f2):
         '''
         Alternation
-        Returns a fragment with a new start state with arrows pointing
+        Returns an NFA with a new start state with arrows pointing
         to both f1 and f2
         '''
-        to_f1 = Arrow('', f1)
-        to_f2 = Arrow('', f2)
-        return Fragment(State(to_f1, to_f2), [to_f1, to_f2])
+        pass
 
     def question(self, f):
         '''
         "?": zero or one
-        Returns a fragment with a new start state pointing to both f
+        Returns an NFA with a new start state pointing to both f
         and the empty path
         '''
         pass
@@ -97,7 +70,7 @@ class NFA(object):
     def star(self):
         '''
         "*": zero or more
-        Returns a fragment with a new start state pointing to both f
+        Returns an NFA with a new start state pointing to both f
         and the empty path, with f.out pointing back to f.start_state
         '''
         pass
@@ -107,14 +80,6 @@ class NFA(object):
         "+": one or more
         '''
         pass
-
-    def patch(self, arrow_list, state):
-        '''
-        Procedure: take a list of arrows and a state and set each arrow
-        to point to state.
-        '''
-        for arrow in arrow_list:
-            arrow.to_state = state
 
     def simulate(self, string):
         '''
@@ -148,22 +113,6 @@ class State(object):
 
     def __str__(self):
         return 'State(out1: {}, out2: {})'.format(self.out1, self.out2)
-
-    def __repr__(self):
-        return self.__str__()
-
-class Fragment(object):
-    '''
-    A fragment is a partial NFA with dangling arrows. The arrows are
-    the set of all out arrows of rightmost states in the fragment.
-    Fragment only tracks a start state and a set of out arrows.
-    '''
-    def __init__(self, start_state, out_arrows=[]):
-        self.start_state = start_state
-        self.out_arrows = out_arrows
-
-    def __str__(self):
-        return 'Fragment(start_state: {}, out_arrows: {})'.format(self.start_state, self.out_arrows)
 
     def __repr__(self):
         return self.__str__()
