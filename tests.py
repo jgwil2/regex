2bb11071e2cbfb95bf60f8c2de8da71f912a79fc
John Willard <jgwil2@gmail.com>
2019-06-19 21:46:17 -0400

    expand tests for machines operating on a single literal character

--- tests.py
+++ tests.py
@@ -5,13 +5,53 @@ from regex import NFA, RegEx
 class TestConcatenation(unittest.TestCase):
 
     def setUp(self):
-        self.nfa = NFA('abc')
+        self.nfa = NFA('ab')
 
     def test_passes_string_in_language(self):
-        self.assertTrue(self.nfa.simulate('abc'))
+        self.assertTrue(self.nfa.simulate('ab'))
 
     def test_fails_string_not_in_language(self):
-        self.assertFalse(self.nfa.simulate('abd'))
+        self.assertFalse(self.nfa.simulate('ac'))
+
+class TestUnion(unittest.TestCase):
+
+    def setUp(self):
+        self.nfa = NFA('a|b')
+
+    def test_passes_string_in_language(self):
+        self.assertTrue(self.nfa.simulate('a'))
+        self.assertTrue(self.nfa.simulate('b'))
+
+    def test_fails_string_not_in_language(self):
+        self.assertFalse(self.nfa.simulate('c'))
+
+class TestStar(unittest.TestCase):
+
+    def setUp(self):
+        self.nfa = NFA('a*')
+
+    def test_passes_string_in_language(self):
+        self.assertTrue(self.nfa.simulate(''))
+        self.assertTrue(self.nfa.simulate('a'))
+        self.assertTrue(self.nfa.simulate('aaa'))
+
+    def test_fails_string_not_in_language(self):
+        self.assertFalse(self.nfa.simulate('ab'))
+        self.assertFalse(self.nfa.simulate('aab'))
+
+class TestPlus(unittest.TestCase):
+
+    def setUp(self):
+        self.nfa = NFA('a+')
+
+    def test_passes_string_in_language(self):
+        self.assertTrue(self.nfa.simulate('a'))
+        self.assertTrue(self.nfa.simulate('aaa'))
+
+    def test_fails_string_not_in_language(self):
+        self.assertTrue(self.nfa.simulate(''))
+        self.assertFalse(self.nfa.simulate('ab'))
+        self.assertFalse(self.nfa.simulate('aab'))
 
 if __name__ == '__main__':
     unittest.main()
