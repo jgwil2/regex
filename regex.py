08bd3f488226530e6c6816d114461b2ea0d3a98f
John Willard <jgwil2@gmail.com>
2019-06-24 22:52:36 -0400

    add concat method, fix simulation algorithm (sort of)

--- regex.py
+++ regex.py
@@ -6,8 +6,9 @@ class Regex(object):
     '''
     def __init__(self, pattern):
         '''
-        Compile an NFA given a regular expression.
+        Compile an NFA given a regular expression pattern
         '''
+        self.nfa = None
 
         for c in pattern:
             if c == '(':
@@ -23,8 +24,11 @@ class Regex(object):
             elif c == '?':
                 self.nfa = NFA.question()
             else:
-                self.nfa = NFA.literal(c)
-
+                # first character
+                if None == self.nfa:
+                    self.nfa = NFA.literal(c)
+                else:
+                    self.nfa = NFA.concat(self.nfa, NFA.literal(c))
 
     def test(self, string):
         return self.nfa.simulate(string)
@@ -38,8 +42,9 @@ class NFA(object):
     The NFA accepts a string if the final state list contains a
     matching state, and rejects it otherwise.
     '''
-    def __init__(self, start_state):
+    def __init__(self, start_state, end_states):
         self.start_state = start_state
+        self.end_states = end_states
 
     @staticmethod
     def literal(c):
@@ -50,14 +55,20 @@ class NFA(object):
         end_state = State(None, None, True)
         edge = Edge(c, end_state)
         start_state = State(edge)
-        return NFA(start_state)
+        return NFA(start_state, [end_state])
 
     @staticmethod
-    def concat(f1, f2):
+    def concat(nfa1, nfa2):
         '''
         Concatenation
         '''
-        pass
+        start_state = nfa1.start_state
+        end_states = nfa2.end_states
+        # FIXME should use immutable data structures
+        for end_state in nfa1.end_states:
+            end_state.is_match = False
+            end_state.out1 = Edge('', nfa2.start_state)
+        return NFA(start_state, end_states)
 
     @staticmethod
     def alternate(f1, f2):
@@ -94,10 +105,22 @@ class NFA(object):
         active_states = [self.start_state]
         next_states = []
         for c in string:
+            # FIXME this code will only follow empty char edges one
+            # level deep. Find a more elegant way to make sure that all
+            # empty char edges are followed!!
             for state in active_states:
-                if state.out1 and (state.out1.char == c or state.out1.char == ''):
+                if state.out1 and state.out1.char == '':
+                    active_states.append(state.out1.to_state)
+                if state.out2 and state.out2.char == '':
+                    active_states.append(state.out1.to_state)
+
+            # after all edges whose char is '' have been followed,
+            # iterate all active states, and if their out edges
+            # match the char, follow the edges
+            for state in active_states:
+                if state.out1 and state.out1.char == c:
                     next_states.append(state.out1.to_state)
-                if state.out2 and (state.out2.char == c or state.out2.char == ''):
+                if state.out2 and state.out2.char == c:
                     next_states.append(state.out2.to_state)
             active_states = next_states
             next_states = []
@@ -118,7 +141,7 @@ class State(object):
         self.is_match = is_match
 
     def __str__(self):
-        return 'State(out1: {}, out2: {})'.format(self.out1, self.out2)
+        return 'State(out1: {}, out2: {}, is_match: {})'.format(self.out1, self.out2, self.is_match)
 
     def __repr__(self):
         return self.__str__()
