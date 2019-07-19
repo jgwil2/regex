7203ca71f3124fbb0067385e8837ddf29db2249f
John Willard <jgwil2@gmail.com>
2019-07-18 23:23:56 -0400

    add tests

--- tests.py
+++ tests.py
@@ -4,9 +4,10 @@ from regex import Regex
 
 class TestExpressionParser(unittest.TestCase):
 
-    def test_insert_concat_operato(self):
+    def test_insert_concat_operator(self):
         self.assertEqual(Regex.insert_concat_operator('ab'), 'a.b')
         self.assertEqual(Regex.insert_concat_operator('abc'), 'a.b.c')
+        self.assertEqual(Regex.insert_concat_operator('ab*'), 'a.b*')
         self.assertEqual(Regex.insert_concat_operator('a((abc)*c)*'), 'a.((a.b.c)*.c)*')
         self.assertEqual(Regex.insert_concat_operator('(ab)*(cd)*'), '(a.b)*.(c.d)*')
         self.assertEqual(Regex.insert_concat_operator('a(bb)+a'), 'a.(b.b)+.a')
@@ -14,6 +15,7 @@ class TestExpressionParser(unittest.TestCase):
     def test_convert_infix_to_post(self):
         self.assertEqual(Regex.convert_infix_to_post('a.b'), 'ab.')
         self.assertEqual(Regex.convert_infix_to_post('a.b.c'), 'ab.c.')
+        self.assertEqual(Regex.convert_infix_to_post('a.b*'), 'ab*.')
         self.assertEqual(Regex.convert_infix_to_post('a.((a.b.c)*.c)*'), 'aab.c.*c.*.')
         self.assertEqual(Regex.convert_infix_to_post('(a.b)*.(c.d)*'), 'ab.*cd.*.')
         self.assertEqual(Regex.convert_infix_to_post('a.(b.b)+.a'), 'abb.+.a.')
@@ -59,6 +61,15 @@ class TestStar(unittest.TestCase):
     def setUp(self):
         self.regex = Regex('a*')
 
+    def test_constructs_correct_machine(self):
+        self.assertTrue(self.regex.nfa.start_state.is_match)
+        self.assertTrue(self.regex.nfa.start_state.out1.char == '')
+        self.assertTrue(self.regex.nfa.start_state.out1.to_state.out1.char == 'a')
+        self.assertTrue(self.regex.nfa.start_state.out1.to_state.out1.to_state.is_match)
+        self.assertTrue(self.regex.nfa.start_state.out1.to_state.out1.to_state.out1.char == '')
+        self.assertTrue(self.regex.nfa.start_state.out1.to_state.out1.to_state.out1.to_state ==
+                        self.regex.nfa.start_state.out1.to_state)
+
     def test_passes_string_in_language(self):
         self.assertTrue(self.regex.test(''))
         self.assertTrue(self.regex.test('a'))
@@ -73,13 +84,26 @@ class TestConcatStar(unittest.TestCase):
     def setUp(self):
         self.regex = Regex('ab*')
 
+    def test_constructs_correct_machine(self):
+        self.assertFalse(self.regex.nfa.start_state.is_match)
+        self.assertTrue(self.regex.nfa.start_state.out1.char == 'a')
+        self.assertTrue(self.regex.nfa.start_state.out1.to_state.out1.char == '')
+        self.assertTrue(self.regex.nfa.start_state.out1.to_state.out1.to_state.is_match)
+        self.assertTrue(self.regex.nfa.start_state.out1.to_state.out1.to_state.out1.char == '')
+        self.assertFalse(self.regex.nfa.start_state.out1.to_state.out1.to_state.out1.to_state.is_match)
+        self.assertTrue(self.regex.nfa.start_state.out1.to_state.out1.to_state.out1.to_state.out1.char == 'b')
+        self.assertTrue(self.regex.nfa.start_state.out1.to_state.out1.to_state.out1.to_state.out1.to_state.is_match)
+        self.assertTrue(self.regex.nfa.start_state.out1.to_state.out1.to_state.out1.to_state.out1.to_state.out1.char ==
+                        '')
+        self.assertTrue(self.regex.nfa.start_state.out1.to_state.out1.to_state.out1.to_state.out1.to_state.out1.to_state ==
+                        self.regex.nfa.start_state.out1.to_state.out1.to_state.out1.to_state)
+
     def test_passes_string_in_language(self):
         self.assertTrue(self.regex.test('a'))
         self.assertTrue(self.regex.test('ab'))
         self.assertTrue(self.regex.test('abb'))
 
     def test_fails_string_not_in_language(self):
-        self.assertFalse(self.regex.test('ab'))
         self.assertFalse(self.regex.test('aab'))
 
 class TestQuestion(unittest.TestCase):
@@ -94,6 +118,20 @@ class TestQuestion(unittest.TestCase):
     def test_fails_string_not_in_language(self):
         self.assertFalse(self.regex.test('aa'))
         self.assertFalse(self.regex.test('ab'))
+        self.assertFalse(self.regex.test('aab'))
+
+class TestConcatQuestion(unittest.TestCase):
+
+    def setUp(self):
+        self.regex = Regex('ab?')
+
+    def test_passes_string_in_language(self):
+        self.assertTrue(self.regex.test('a'))
+        self.assertTrue(self.regex.test('ab'))
+        self.assertTrue(self.regex.test('ab'))
+
+    def test_fails_string_not_in_language(self):
+        self.assertFalse(self.regex.test('abb'))
         self.assertFalse(self.regex.test('aab'))
 
 class TestPlus(unittest.TestCase):
